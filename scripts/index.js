3cb2605d5f6baaffa66e580dd95fa19285d3872e
shuanllene <106447557+shuanllene@users.noreply.github.com>
2022-08-16 09:02:50 -0400

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,85 @@
+var qaList = [];
+
+function loadQA(q, a) {
+    var obj = {
+        question: q,
+        answer: a,
+    };
+    return obj
+}
+
+function callQA() {
+    var question = document.getElementById("question").value;
+    var answer = document.getElementById("answer").value;
+    if (question.trim()=='' || answer.trim()== '') {
+        alert('Question or answer CANNOT be empty!');
+    } else {
+        var qaObj = loadQA(question, answer);
+        qaList.push(qaObj);
+        document.getElementById("question").value = '';
+        document.getElementById("answer").value = '';
+        document.getElementById('formMessage').innerHTML = `Data loaded to QA Array. System now holding ${qaList.length} QA objects.`;
+    }
+
+}
+
+function qRender(i){
+    document.getElementById('questionArea').innerHTML = `Question: ${qaList[i].question}`;
+    document.getElementById('answerArea').innerHTML = 'Answer:';
+    document.getElementById('questionArea').qIndex = i;
+    document.getElementById('questionArea').showAnswer = false; 
+}
+
+function aRender(){
+    i = document.getElementById('questionArea').qIndex;
+    ifShown = document.getElementById('questionArea').showAnswer
+    if (ifShown) {
+        document.getElementById('answerArea').innerHTML = 'Answer:';
+        document.getElementById('questionArea').showAnswer = false;
+    } else {
+        document.getElementById('answerArea').innerHTML = `Answer: ${qaList[i].answer}`;
+        document.getElementById('questionArea').showAnswer = true;
+    }
+}
+
+function qaDisplay(){
+    if (qaList.length == 0) {
+        document.getElementById('questionArea').innerHTML = 'No QA data found! Please input you data.';
+    } else {
+        var i = 0
+        document.getElementById("nextQuestion").removeAttribute("hidden");
+        document.getElementById("prevQuestion").removeAttribute("hidden");;
+        document.getElementById("showAnswer").removeAttribute("hidden");
+        qRender(i);
+    }
+}
+
+function nextQuestion(){
+    i = document.getElementById('questionArea').qIndex;
+    if (i == qaList.length - 1) {
+        i = 0;
+    } else {
+        i++;
+    }
+    qRender(i);
+}
+
+function prevQuestion(){
+    i = document.getElementById('questionArea').qIndex;
+    if (i == 0) {
+        i = qaList.length - 1;
+    } else {
+        i--;
+    }
+    qRender(i);
+}
+
+function qaConsole(){
+    var i = 0
+    do {
+        console.log('question:', qaList[i].question);
+        console.log('answer:', qaList[i].answer);
+        i++;
+    } while (i < qaList.length);
+    document.getElementById('qaInConsole').innerHTML = `${qaList.length} QA pairs are printed in console.`
+}
